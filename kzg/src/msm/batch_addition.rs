014b889e4da2462068de01e5109c4dd748ed82a0
Artiom Tretjakovas <artiom@grandine.io>
2026-03-17 16:57:51 +0200

    Optimize batch addition performance
    Batch addition performance was optimized while covering necessary edge
    cases. Now, batch addition handles infinity points (and combinations)
    without performance regressions.

--- kzg/src/msm/batch_addition.rs
+++ kzg/src/msm/batch_addition.rs
@@ -1,3 +1,7 @@
+//! This algorithm is taken from crate-crypto's [rust-eth-kzg], adapted and
+//! optimized for our usecase.
+//!
+//! [rust-eth-kzg]: https://github.com/crate-crypto/rust-eth-kzg/blob/acf3a5e8062e9a67c184c77ebff9c55223f9e034/crates/cryptography/bls12_381/src/batch_addition.rs
 use alloc::{vec, vec::Vec};
 
 use crate::{G1Affine, G1Fp, G1ProjAddAffine, G1};
@@ -7,21 +11,28 @@ use crate::{G1Affine, G1Fp, G1ProjAddAffine, G1};
 const BATCH_INVERSE_THRESHOLD: usize = 16;
 
 /// Chooses between point addition and point doubling based on the input points.
-///
-/// Note: This does not handle the case where p1 == -p2.
-///
-/// This case is unlikely for our usecase, and is not trivial
-/// to handle.
 #[inline(always)]
 fn choose_add_or_double<TG1: G1, TG1Fp: G1Fp, TG1Affine: G1Affine<TG1, TG1Fp>>(
-    p1: TG1Affine,
-    p2: TG1Affine,
+    pair: &mut [TG1Affine],
 ) -> TG1Fp {
-    if p1 == p2 {
-        p2.y().double()
+    let fp = if pair[0].is_infinity() || pair[1].is_infinity() {
+        return TG1Fp::one();
+    } else if pair[0].x() == pair[1].x() {
+        if pair[0].y() != pair[1].y() {
+            pair[1] = TG1Affine::zero();
+            pair[0] = TG1Affine::zero();
+            return TG1Fp::one();
+        }
+
+        let f = pair[1].y().double();
+        *pair[1].y_mut() = pair[0].x().square().mul3();
+        f
     } else {
-        p2.x().sub_fp(p1.x())
-    }
+        *pair[1].y_mut() = pair[1].y().sub_fp(pair[0].y());
+        pair[1].x().sub_fp(pair[0].x())
+    };
+
+    fp
 }
 
 /// Adds two elliptic curve points using the point addition/doubling formula.
@@ -35,11 +46,15 @@ fn point_add_double<TG1: G1, TG1Fp: G1Fp, TG1Affine: G1Affine<TG1, TG1Fp>>(
     p2: TG1Affine,
     inv: &TG1Fp,
 ) -> TG1Affine {
-    let lambda = if p1 == p2 {
-        p1.x().square().mul3().mul_fp(inv)
-    } else {
-        p2.y().sub_fp(p1.y()).mul_fp(inv)
-    };
+    if p1.is_zero() {
+        return p2;
+    }
+
+    if p2.is_zero() {
+        return p1;
+    }
+
+    let lambda = p2.y().mul_fp(inv);
 
     let x = lambda.square().sub_fp(p1.x()).sub_fp(p2.x());
     let y = lambda.mul_fp(&p1.x().sub_fp(&x)).sub_fp(p1.y());
@@ -54,6 +69,10 @@ fn point_add_double<TG1: G1, TG1Fp: G1Fp, TG1Affine: G1Affine<TG1, TG1Fp>>(
 ///
 /// Panics if any of the elements are zero
 pub fn batch_inverse_scratch_pad<F: G1Fp>(v: &mut [F], scratchpad: &mut Vec<F>) {
+    if v.is_empty() {
+        return;
+    }
+
     // Montgomery's Trick and Fast Implementation of Masked AES
     // Genelle, Prouff and Quisquater
     // Section 3.2
@@ -67,7 +86,7 @@ pub fn batch_inverse_scratch_pad<F: G1Fp>(v: &mut [F], scratchpad: &mut Vec<F>) 
     let mut tmp = F::one();
     for f in v.iter() {
         tmp = tmp.mul_fp(f);
-        scratchpad.push(tmp.clone());
+        scratchpad.push(tmp);
     }
 
     // Invert `tmp`.
@@ -76,18 +95,14 @@ pub fn batch_inverse_scratch_pad<F: G1Fp>(v: &mut [F], scratchpad: &mut Vec<F>) 
         .expect("guaranteed to be non-zero since we filtered out zero field elements");
 
     // Second pass: iterate backwards to compute inverses
-    for (f, s) in v
-        .iter_mut()
-        // Backwards
-        .rev()
-        // Backwards, skip last element, fill in one for last term.
-        .zip(scratchpad.iter().rev().skip(1).chain(Some(&F::one())))
-    {
-        // tmp := tmp * f; f := tmp * s = 1/f
-        let new_tmp = tmp.mul_fp(f);
-        *f = tmp.mul_fp(s);
+    for i in (1..v.len()).rev() {
+        // tmp := tmp * v[i]; v[i] := tmp * scratchpad[i - 1] = 1 / v[i]
+        let new_tmp = tmp.mul_fp(&v[i]);
+        v[i] = tmp.mul_fp(&scratchpad[i - 1]);
         tmp = new_tmp;
     }
+
+    v[0] = tmp;
 }
 
 /// Performs multi-batch addition of multiple sets of elliptic curve points.
@@ -103,65 +118,38 @@ pub fn multi_batch_addition_binary_tree_stride<
 >(
     mut multi_points: Vec<Vec<TG1Affine>>,
 ) -> Vec<TG1> {
-    multi_points
-        .iter_mut()
-        .for_each(|points| points.retain(|p| !p.is_infinity()));
-    let total_num_points: usize = multi_points.iter().map(|p| p.len()).sum();
+    let mut total_num_points = 0;
+    let mut total_amount_of_work = 0;
+
+    for points in &multi_points {
+        total_num_points += points.len();
+        total_amount_of_work += points.len() / 2;
+    }
+
     let mut scratchpad = Vec::with_capacity(total_num_points);
-
-    // Find the largest buckets, this will be the bottleneck for the number of iterations
-    let mut max_bucket_length = 0;
-    for points in multi_points.iter() {
-        max_bucket_length = std::cmp::max(max_bucket_length, points.len());
-    }
-
-    // Compute the total number of "unit of work"
-    // In the single batch addition case this is analogous to
-    // the batch inversion threshold
-    #[inline(always)]
-    fn compute_threshold<TG1: G1, TG1Fp: G1Fp, TG1Affine: G1Affine<TG1, TG1Fp>>(
-        points: &[Vec<TG1Affine>],
-    ) -> usize {
-        points
-            .iter()
-            .map(|p| {
-                if p.len() % 2 == 0 {
-                    p.len() / 2
-                } else {
-                    (p.len() - 1) / 2
-                }
-            })
-            .sum()
-    }
-
-    let mut denominators = Vec::with_capacity(max_bucket_length);
-    let mut total_amount_of_work = compute_threshold(&multi_points);
-
+    let mut denominators = Vec::with_capacity(total_num_points / 2);
     let mut sums = vec![TG1::identity(); multi_points.len()];
 
     // TODO: total_amount_of_work does not seem to be changing performance that much
     while total_amount_of_work > BATCH_INVERSE_THRESHOLD {
-        // For each point, we check if they are odd and pop off
-        // one of the points
-        for (points, sum) in multi_points.iter_mut().zip(sums.iter_mut()) {
-            // Make the number of points even
-            if points.len() % 2 != 0 {
-                TG1ProjAddAffine::add_or_double_assign_affine(sum, &points.pop().unwrap());
-            }
-        }
-
+        total_amount_of_work = 0;
         denominators.clear();
 
         // For each pair of points over all
         // vectors, we collect them and put them in the
         // inverse array
-        for points in multi_points.iter_mut() {
+        for (points, sum) in multi_points.iter_mut().zip(sums.iter_mut()) {
+            // Make the number of points even
+            if points.len() % 2 != 0 {
+                TG1ProjAddAffine::add_or_double_assign_affine(sum, &points.pop().unwrap());
+            }
+
             if points.len() < 2 {
                 continue;
             }
 
-            for i in (0..=points.len() - 2).step_by(2) {
-                denominators.push(choose_add_or_double(points[i], points[i + 1]));
+            for pair in points.chunks_exact_mut(2) {
+                denominators.push(choose_add_or_double(pair));
             }
         }
 
@@ -170,13 +158,6 @@ pub fn multi_batch_addition_binary_tree_stride<
         let mut denominators_offset = 0;
 
         for points in multi_points.iter_mut() {
-            *points = points
-                .chunks_exact(2)
-                .filter(|v| v[0] != v[1].neg())
-                .flat_map(|v| v)
-                .cloned()
-                .collect::<Vec<_>>();
-
             if points.len() < 2 {
                 continue;
             }
@@ -185,19 +166,16 @@ pub fn multi_batch_addition_binary_tree_stride<
                 .step_by(2)
                 .zip(&denominators[denominators_offset..])
             {
-                let p1 = points[i];
-                let p2 = points[i + 1];
-                points[i / 2] = point_add_double(p1, p2, inv);
+                points[i / 2] = point_add_double(points[i], points[i + 1], inv);
             }
 
             let num_points = points.len() / 2;
             // The latter half of the vector is now unused,
             // all results are stored in the former half.
             points.truncate(num_points);
-            denominators_offset += num_points
+            denominators_offset += num_points;
+            total_amount_of_work += num_points / 2;
         }
-
-        total_amount_of_work = compute_threshold(&multi_points);
     }
 
     for (sum, points) in sums.iter_mut().zip(multi_points) {
